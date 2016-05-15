a1729d2919f9aea916f503a4c95a74b1a592215c
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-05-15 16:43:01 -0400

    6.3 functional tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+
+		before do
+			@user = User.new(email: "test@test.com", password: "pasta")
+		end
+
+		describe "GET #show" do
+				
+			context "User is logged in" do
+				before do
+					sign_in @user
+				end
+
+				it "loads correct user details"
+					get :show, id: @user.id
+					expect(response).to have_http_status(200)
+					expect(assigns(:user)).to eq @user
+				end
+			end
+
+				
+			context "No user is logged in" do
+				it "redirects to login" do
+					get :show, id: @user.id
+					expect(response).to redirect_to(root_path)
+				end
+
+			end
+
+			context "Cannot access second user show page" do
+				it "redirects to root" do
+					get :show, id: @user_second.id
+					expect(response).to redirect_to(root_path)
+				end
+			end
+		end
