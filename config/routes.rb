4bf4ebe13ee407bc07c0153635647bdc06e5bc4a
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-05-22 13:55:32 -0400

    6.8 creating payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
+  resources :payments
+  resources :charges
   resources :products
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  post 'payments/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
