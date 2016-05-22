61686c7c6cfde710e286ba730eacc706805cc524
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-05-22 18:56:55 -0400

    6.11 jQuery

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,5 @@
 $(document).on('ready page:load', function(){
+	$('.img-zoom').elevateZoom();
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty( { path: '/assets',
 		readOnly: true,
