1fd2b0f3bc46b78ab1c2c60c3ee796584775603b
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-05-30 21:39:05 -0400

    fixing jquery zoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,8 @@
 $(document).on('ready page:load', function(){
-	$('.img-zoom').elevateZoom();
+	$('.img-zoom').elevateZoom({
+		zoomWindowWidth:100,
+		zoomWindowHeight:100
+});
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty( { path: '/assets',
 		readOnly: true,
