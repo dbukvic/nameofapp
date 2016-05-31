bdf2fc7157f38ced33dbbfe1f739c0b49f42eb49
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-05-31 19:31:52 -0400

    Trying to figure out elevatezoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,9 @@
 $(document).on('ready page:load', function(){
-	$('.img-zoom').elevateZoom();
+	$('.img-zoom').elevateZoom({
+		zoomWindowWidth: 100,
+		zoomWindowHeight: 100
+
+	});
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty( { path: '/assets',
 		readOnly: true,
