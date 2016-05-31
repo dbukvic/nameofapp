dfeeec16590ba08daa08a1cfcc28ecd9cad83ac5
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-05-30 22:00:11 -0400

    fixed jquery zoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,8 @@
 $(document).on('ready page:load', function(){
 	$('.img-zoom').elevateZoom({
 		zoomWindowWidth:100,
-		zoomWindowHeight:100
+		zoomWindowHeight:100,
+		width:330,
 });
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty( { path: '/assets',
