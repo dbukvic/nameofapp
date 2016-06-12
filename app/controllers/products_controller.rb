f6cf3eba7249294556ca71073b733a73bd15c9ad
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-06-12 13:32:46 -0400

    fixes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
   respond_to :json, :html
 
   # GET /products
   # GET /products.json
+
   def index
     if params[:q]
       search_term = params[:q]
