531966ba14e70e95e9f25faa293368cd06b5314c
Danielle Bukvic <bukvic.danielle@gmail.com>
2016-06-13 11:31:11 -0400

    Just admins should be able to delete products

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,12 @@
 class Ability
   include CanCan::Ability
-    
-    
-    def initialize(user)
-      
-      # user ||= User.new #guest user (not logged in)
-      can :manage, User, id: user.id
-      
-        if user.admin?
-         can :manage, :all
-        else
-         can :read, :all
-        end
-    end
 
+  def initialize(user)
+    user ||= User.new # guest user
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+    end
+  end
 end
